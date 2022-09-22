84a4101a2851d7c6bc16670799d59825dc0f769a
patrickpato <ningipatrick@gmail.com>
2022-09-21 21:30:50 -0700

    marching on strong

---
+++ app.js
@@ -0,0 +1,51 @@
+ //declaring variables
+ const cartBtn = document.querySelector(".cart-btn");
+ const closeCartBtn = document.querySelector(".close-cart");
+ const clearCartBtn = document.querySelector(".clear-cart");
+ const cartDOM = document.querySelector(".cart");
+ const cartOverlay = document.querySelector(".cart-overaly");
+ const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center"); 
+
+//main cart 
+let cart = [];
+
+//getting the product
+class Products{
+    async getProducts(){
+        try {
+            let result = await fetch("products.json");
+            let data = await result.json();
+            let products = data.items;
+            products = products.map(item =>{
+                const {title, price} = item.fields;
+                const {id} = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {title, price, id, image}
+            })
+            return products;
+        }
+        catch (error){
+            console.log(error);
+        }
+    }
+
+    // STOPPED AT 1HR 34 MINS
+}
+//displaying products 
+class UI{
+
+}
+
+//local storage 
+class Storage{ 
+
+}
+document.addEventListener("DOMContentLoaded", ()=> {
+    const ui = new UI();
+    const products = new Products();
+    //get all products
+    products.getProducts().then(data => console.log(data))
+})
